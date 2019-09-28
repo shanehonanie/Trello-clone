a9790c1d4f057b4013940c86383870b165e8941c
Shane Honanie <shaneh425@gmail.com>
2019-09-28 14:41:18 -0700

    Project Object Validation

--- PPMToolFullStack/src/main/java/io/agileintelligence/ppmtool/domain/Project.java
+++ PPMToolFullStack/src/main/java/io/agileintelligence/ppmtool/domain/Project.java
@@ -1,6 +1,10 @@
 package io.agileintelligence.ppmtool.domain;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import java.util.Date;
 
 @Entity
@@ -9,13 +13,22 @@ public class Project {
     @Id
     @GeneratedValue(strategy= GenerationType.IDENTITY)
     private Long id;
+    @NotBlank(message = "Project name is required")
     private String projectName;
+    @NotBlank(message = "Project Identifier is required")
+    @Size(min = 4, max=5, message="please use 4 to 5 characters")
+    @Column(updatable = false, unique = true)
     private String projectIdentifier;
+    @NotBlank(message="Project description is required")
     private String description;
-    private java.util.Date start_date;
+    @JsonFormat(pattern = "yyyy-mm-dd")
+    private Date start_date;
+    @JsonFormat(pattern = "yyyy-mm-dd")
     private Date end_date;
 
+    @JsonFormat(pattern = "yyyy-mm-dd")
     private Date created_At;
+    @JsonFormat(pattern = "yyyy-mm-dd")
     private Date updated_At;
 
     public Project() {
