83899c27cb1dac14251bf8c01012276e3a531151
Shane Honanie <shaneh425@gmail.com>
2019-09-28 17:10:34 -0700

    Find Project by ID

--- PPMToolFullStack/src/main/java/io/agileintelligence/ppmtool/repositories/ProjectRepository.java
+++ PPMToolFullStack/src/main/java/io/agileintelligence/ppmtool/repositories/ProjectRepository.java
@@ -6,6 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ProjectRepository extends CrudRepository<Project, Long> {
-    @Override
-    Iterable<Project> findAllById(Iterable<Long> longs);
+    Project findByProjectIdentifier(String projectId);
 }
--- PPMToolFullStack/src/main/java/io/agileintelligence/ppmtool/services/ProjectService.java
+++ PPMToolFullStack/src/main/java/io/agileintelligence/ppmtool/services/ProjectService.java
@@ -20,4 +20,13 @@ public class ProjectService {
             throw new ProjectIdException("Project ID: " + project.getProjectIdentifier().toUpperCase() + " -- already exists");
         }
     }
+
+    public Project findProjectByIdentifier(String projectId) {
+        Project project = projectRepository.findByProjectIdentifier(projectId.toUpperCase());
+
+        if(project == null)
+            throw new ProjectIdException("Project does not exist");
+
+        return project;
+    }
 }
