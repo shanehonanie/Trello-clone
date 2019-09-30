40c0ceff6225371de27bf2165f5718ba9e6b2233
Shane Honanie <shaneh425@gmail.com>
2019-09-29 23:22:39 -0700

    React Router/addProject

--- ppmtool-react-client/src/App.js
+++ ppmtool-react-client/src/App.js
@@ -3,13 +3,18 @@ import './App.css';
 import Dashboard from './components/Dashboard';
 import Header from './components/Layout/Header';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter, Route } from 'react-router-dom';
+import AddProject from './components/Project/AddProject';
 
 function App() {
 	return (
-		<div className='App'>
-			<Header />
-			<Dashboard />
-		</div>
+		<BrowserRouter>
+			<div className='App'>
+				<Header />
+				<Route exact path='/dashboard' component={Dashboard} />
+				<Route exact path='/addProject' component={AddProject} />
+			</div>
+		</BrowserRouter>
 	);
 }
 
