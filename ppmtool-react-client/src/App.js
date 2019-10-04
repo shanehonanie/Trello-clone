495d69582b2656ebdf3cf6125ac2997fe627124c
Shane Honanie <shaneh425@gmail.com>
2019-10-04 13:17:34 -0700

    Layout for ProjectBoard/Tasks

--- ppmtool-react-client/src/App.js
+++ ppmtool-react-client/src/App.js
@@ -9,6 +9,8 @@ import Dashboard from './components/Dashboard';
 import Header from './components/Layout/Header';
 import AddProject from './components/Project/AddProject';
 import UpdateProject from './components/Project/UpdateProject';
+import ProjectBoard from './components/ProjectBoard/ProjectBoard';
+import AddProjectTask from './components/ProjectBoard/ProjectTasks/AddProjectTask';
 
 function App() {
 	return (
@@ -19,6 +21,8 @@ function App() {
 					<Route exact path='/dashboard' component={Dashboard} />
 					<Route exact path='/addProject' component={AddProject} />
 					<Route exact path='/updateProject/:id' component={UpdateProject} />
+					<Route exact path='/projectBoard/:id' component={ProjectBoard} />
+					<Route exact path='/addProjectTask/:id' component={AddProjectTask} />
 				</div>
 			</BrowserRouter>
 		</Provider>
