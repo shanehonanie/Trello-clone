0cdab5e6fd7d7c481335e3730a2dffaa8f0c54b9
Shane Honanie <shaneh425@gmail.com>
2019-10-03 19:00:49 -0700

    Backlog Reducer

---
+++ ppmtool-react-client/src/reducers/backlogReducer.js
@@ -0,0 +1,33 @@
+import {
+	GET_BACKLOG,
+	GET_PROJECT_TASK,
+	DELETE_PROJECT_TASK
+} from '../actions/types';
+
+const initialState = {
+	projectTasks: [],
+	projectTask: {}
+};
+
+export default function(state = initialState, action) {
+	switch (action.type) {
+		case GET_BACKLOG:
+			return {
+				...state,
+				projectTasks: action.payload
+			};
+		case GET_PROJECT_TASK:
+			return {
+				...state,
+				projectTask: action.payload
+			};
+		case DELETE_PROJECT_TASK:
+			return {
+				...state
+				// TODO map filter
+			};
+
+		default:
+			return state;
+	}
+}
