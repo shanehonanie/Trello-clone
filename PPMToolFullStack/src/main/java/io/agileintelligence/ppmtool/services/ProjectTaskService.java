d89e7bcb20f16435cfed36df0463069efcc058aa
Shane Honanie <shaneh425@gmail.com>
2019-10-03 18:19:14 -0700

    PATCH Update Project Task

--- PPMToolFullStack/src/main/java/io/agileintelligence/ppmtool/services/ProjectTaskService.java
+++ PPMToolFullStack/src/main/java/io/agileintelligence/ppmtool/services/ProjectTaskService.java
@@ -72,11 +72,19 @@ public class ProjectTaskService {
         if(projectTask == null) {
             throw new ProjectNotFoundException("Project Task " + pt_id + " not found");
         }
-        // make sure that back/project in path coresponds to the right project
+        // make sure that back/project in path corresponds to the right project
         if(!projectTask.getProjectIdentifier().equals(backlog_id)) {
             throw new ProjectNotFoundException("Project Task " + pt_id + " does not exist in project " + backlog_id);
         }
 
         return projectTask;
+    }
+
+    public ProjectTask updateByProjectSequence(ProjectTask updatedTask, String backlog_id, String pt_id){
+        ProjectTask projectTask = findPTByProjectSequence(backlog_id, pt_id);
+
+        projectTask = updatedTask;
+
+        return projectTaskRepository.save(projectTask);
     }
 }
