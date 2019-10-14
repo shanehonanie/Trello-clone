0dc36d238d8be46d5d6a4db7eed4a2b69da7808b
Shane Honanie <shaneh425@gmail.com>
2019-10-13 18:33:32 -0700

    Create User Validation/Repo/Service

---
+++ PPMToolFullStack/src/main/java/io/agileintelligence/ppmtool/security/UserService.java
@@ -0,0 +1,11 @@
+package io.agileintelligence.ppmtool.security;
+
+import io.agileintelligence.ppmtool.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+}
