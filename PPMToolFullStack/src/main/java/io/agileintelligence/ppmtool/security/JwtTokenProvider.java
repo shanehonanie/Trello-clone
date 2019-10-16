8e395f458c1010e07e1a06f696d943303fdbf985
Shane Honanie <shaneh425@gmail.com>
2019-10-15 17:13:13 -0700

    Custom JWT Filter for Authorization

--- PPMToolFullStack/src/main/java/io/agileintelligence/ppmtool/security/JwtTokenProvider.java
+++ PPMToolFullStack/src/main/java/io/agileintelligence/ppmtool/security/JwtTokenProvider.java
@@ -1,9 +1,7 @@
 package io.agileintelligence.ppmtool.security;
 
 import io.agileintelligence.ppmtool.domain.User;
-import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.*;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
 
@@ -37,6 +35,29 @@ public class JwtTokenProvider {
     }
 
     // Validate Token
+    public boolean validateToken(String token) {
+        try{
+            Jwts.parser().setSigningKey(SECRET).parseClaimsJws(token);
+            return true;
+        }catch (SignatureException ex) {
+            System.out.println("Invalid JWT Signature");
+        } catch (MalformedJwtException ex) {
+            System.out.println("Invalid JWT Token");
+        } catch (ExpiredJwtException ex) {
+            System.out.println("Expired JWT Token");
+        } catch (UnsupportedJwtException ex) {
+            System.out.println("Unsupported JWT Token");
+        } catch (IllegalArgumentException ex) {
+            System.out.println("JWT claims string is empty");
+        }
+        return false;
+    }
 
     // Get user Id from token
+    public Long getUserIdFromJWT(String token) {
+        Claims claims = Jwts.parser().setSigningKey(SECRET).parseClaimsJws(token).getBody();
+        String id = (String) claims.get("id");
+
+        return Long.parseLong(id);
+    }
 }
--- PPMToolFullStack/src/main/java/io/agileintelligence/ppmtool/security/SecurityConfig.java
+++ PPMToolFullStack/src/main/java/io/agileintelligence/ppmtool/security/SecurityConfig.java
@@ -13,6 +13,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import static io.agileintelligence.ppmtool.security.SecurityConstants.H2_URL;
 import static io.agileintelligence.ppmtool.security.SecurityConstants.SIGN_UP_URLS;
@@ -30,6 +31,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     private CustomUserDetailsService customUserDetailsService;
+
+    @Bean
+    public JwtAuthenticationFilter jwtAuthenticationFilter() {return new JwtAuthenticationFilter();}
 
     @Autowired
     private BCryptPasswordEncoder bCryptPasswordEncoder;
@@ -69,5 +73,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
             .antMatchers(SIGN_UP_URLS).permitAll()
             .antMatchers(H2_URL).permitAll()
             .anyRequest().authenticated();
+
+        http.addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
     }
 }
