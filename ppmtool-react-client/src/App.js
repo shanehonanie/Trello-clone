6aad3f0ff9350e0d71ceac81d972d18511c88c69
Shane Honanie <shaneh425@gmail.com>
2019-10-19 22:20:30 -0700

    Landing, Login , and Register

--- ppmtool-react-client/src/App.js
+++ ppmtool-react-client/src/App.js
@@ -7,11 +7,14 @@ import { Provider } from 'react-redux';
 import store from './store';
 import Dashboard from './components/Dashboard';
 import Header from './components/Layout/Header';
+import Landing from './components/Layout/Landing';
 import AddProject from './components/Project/AddProject';
 import UpdateProject from './components/Project/UpdateProject';
 import ProjectBoard from './components/ProjectBoard/ProjectBoard';
 import AddProjectTask from './components/ProjectBoard/ProjectTasks/AddProjectTask';
 import UpdateProjectTask from './components/ProjectBoard/ProjectTasks/UpdateProjectTask';
+import Register from './components/UserManagement/Register';
+import Login from './components/UserManagement/Login';
 
 function App() {
 	return (
@@ -19,6 +22,16 @@ function App() {
 			<BrowserRouter>
 				<div className='App'>
 					<Header />
+					{
+						// Public Routes
+					}
+
+					<Route exact path='/' component={Landing} />
+					<Route exact path='/register' component={Register} />
+					<Route exact path='/login' component={Login} />
+					{
+						// Private Routes
+					}
 					<Route exact path='/dashboard' component={Dashboard} />
 					<Route exact path='/addProject' component={AddProject} />
 					<Route exact path='/updateProject/:id' component={UpdateProject} />
