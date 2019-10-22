4115f31864aefdc19651ebd2e91a0efa48270092
Shane Honanie <shaneh425@gmail.com>
2019-10-21 17:38:03 -0700

    Fix Update Project Task null Date

--- PPMToolFullStack/src/main/java/io/agileintelligence/ppmtool/domain/ProjectTask.java
+++ PPMToolFullStack/src/main/java/io/agileintelligence/ppmtool/domain/ProjectTask.java
@@ -1,5 +1,6 @@
 package io.agileintelligence.ppmtool.domain;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import javax.persistence.*;
@@ -18,6 +19,7 @@ public class ProjectTask {
     private String acceptanceCriteria;
     private String status;
     private Integer priority;
+    @JsonFormat(pattern = "yyyy-mm-dd")
     private Date dueDate;
 
     // ManyToOne with backlog
@@ -28,7 +30,9 @@ public class ProjectTask {
 
     @Column(updatable = false)
     private String projectIdentifier;
+    @JsonFormat(pattern = "yyyy-mm-dd")
     private Date create_At;
+    @JsonFormat(pattern = "yyyy-mm-dd")
     private Date update_At;
 
     @PrePersist
