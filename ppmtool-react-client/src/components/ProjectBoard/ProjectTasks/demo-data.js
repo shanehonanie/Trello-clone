142559922b3b4c8a525b34dab52d93779b2e6adf
Shane Honanie <shaneh425@gmail.com>
2020-01-06 16:52:36 -0800

    Demo Data for Landing Page

---
+++ ppmtool-react-client/src/components/ProjectBoard/ProjectTasks/demo-data.js
@@ -0,0 +1,241 @@
+const data = {
+	tasks: [
+		{
+			id: 57,
+			projectSequence: 'TDEV-1',
+			summary: 'Implement Search Functionlity',
+			acceptanceCriteria: '',
+			status: 'TODO',
+			priority: 3,
+			dueDate: '2020-01-07',
+			projectIdentifier: 'TDEV',
+			create_At: '2020-05-06',
+			update_At: null
+		},
+		{
+			id: 58,
+			projectSequence: 'TDEV-2',
+			summary: 'Implement Authentication',
+			acceptanceCriteria: '',
+			status: 'DONE',
+			priority: 3,
+			dueDate: '2020-01-01',
+			projectIdentifier: 'TDEV',
+			create_At: '2020-05-06',
+			update_At: null
+		},
+		{
+			id: 59,
+			projectSequence: 'TDEV-3',
+			summary: 'Write Server JUnit Tests',
+			acceptanceCriteria: '',
+			status: 'IN_PROGRESS',
+			priority: 3,
+			dueDate: '2020-01-08',
+			projectIdentifier: 'TDEV',
+			create_At: '2020-06-06',
+			update_At: null
+		},
+		{
+			id: 60,
+			projectSequence: 'TDEV-4',
+			summary: 'Implement Changing of Background Color By User',
+			acceptanceCriteria: '',
+			status: 'TODO',
+			priority: 3,
+			dueDate: '2020-01-10',
+			projectIdentifier: 'TDEV',
+			create_At: '2020-06-06',
+			update_At: null
+		},
+		{
+			id: 61,
+			projectSequence: 'TDEV-5',
+			summary: 'Implement Navbar',
+			acceptanceCriteria: '',
+			status: 'DONE',
+			priority: 3,
+			dueDate: '2020-01-01',
+			projectIdentifier: 'TDEV',
+			create_At: '2020-06-06',
+			update_At: null
+		},
+		{
+			id: 62,
+			projectSequence: 'TDEV-6',
+			summary: 'Update CSS for List Appearance (Hover and Drag & Drop Events)',
+			acceptanceCriteria: '',
+			status: 'DONE',
+			priority: 3,
+			dueDate: '2020-01-06',
+			projectIdentifier: 'TDEV',
+			create_At: '2020-07-06',
+			update_At: '2020-38-06'
+		},
+		{
+			id: 63,
+			projectSequence: 'TDEV-7',
+			summary: 'Implement Adding & Removing Custom Lists',
+			acceptanceCriteria: '',
+			status: 'TODO',
+			priority: 3,
+			dueDate: '2020-01-07',
+			projectIdentifier: 'TDEV',
+			create_At: '2020-09-06',
+			update_At: null
+		},
+		{
+			id: 64,
+			projectSequence: 'TDEV-8',
+			summary:
+				'Implement Temporary Modals for Add/Edit/Delete of Projects and Tasks',
+			acceptanceCriteria: '',
+			status: 'DONE',
+			priority: 3,
+			dueDate: '2020-01-05',
+			projectIdentifier: 'TDEV',
+			create_At: '2020-09-06',
+			update_At: null
+		},
+		{
+			id: 65,
+			projectSequence: 'TDEV-9',
+			summary: 'Implement Responsiveness for Smaller Screen Sizes',
+			acceptanceCriteria: '',
+			status: 'TODO',
+			priority: 3,
+			dueDate: '2020-01-06',
+			projectIdentifier: 'TDEV',
+			create_At: '2020-10-06',
+			update_At: '2020-21-06'
+		},
+		{
+			id: 66,
+			projectSequence: 'TDEV-10',
+			summary: 'Implement Navbar Dropdown for Boards and Logout',
+			acceptanceCriteria: '',
+			status: 'DONE',
+			priority: 3,
+			dueDate: '2020-01-04',
+			projectIdentifier: 'TDEV',
+			create_At: '2020-11-06',
+			update_At: null
+		},
+		{
+			id: 67,
+			projectSequence: 'TDEV-11',
+			summary: 'Refactor MySQL Schema',
+			acceptanceCriteria: '',
+			status: 'IN_PROGRESS',
+			priority: 3,
+			dueDate: '2020-01-08',
+			projectIdentifier: 'TDEV',
+			create_At: '2020-12-06',
+			update_At: '2020-30-06'
+		},
+		{
+			id: 68,
+			projectSequence: 'TDEV-12',
+			summary: 'Refactor Java Spring Controllers',
+			acceptanceCriteria: '',
+			status: 'IN_PROGRESS',
+			priority: 3,
+			dueDate: '2020-01-06',
+			projectIdentifier: 'TDEV',
+			create_At: '2020-12-06',
+			update_At: '2020-39-06'
+		},
+		{
+			id: 69,
+			projectSequence: 'TDEV-13',
+			summary: 'Implement Drag & Drop of Tasks',
+			acceptanceCriteria: '',
+			status: 'DONE',
+			priority: 3,
+			dueDate: '2020-01-04',
+			projectIdentifier: 'TDEV',
+			create_At: '2020-19-06',
+			update_At: null
+		},
+		{
+			id: 70,
+			projectSequence: 'TDEV-14',
+			summary: 'Refactor React / Redux',
+			acceptanceCriteria: '',
+			status: 'DONE',
+			priority: 3,
+			dueDate: '2020-01-05',
+			projectIdentifier: 'TDEV',
+			create_At: '2020-20-06',
+			update_At: '2020-20-06'
+		},
+		{
+			id: 71,
+			projectSequence: 'TDEV-15',
+			summary: 'Update CSS for Edit Modals',
+			acceptanceCriteria: '',
+			status: 'IN_PROGRESS',
+			priority: 3,
+			dueDate: '2020-01-05',
+			projectIdentifier: 'TDEV',
+			create_At: '2020-21-06',
+			update_At: '2020-39-06'
+		},
+		{
+			id: 72,
+			projectSequence: 'TDEV-16',
+			summary: 'Add Demo to Main Page',
+			acceptanceCriteria: '',
+			status: 'DONE',
+			priority: 3,
+			dueDate: '2020-01-05',
+			projectIdentifier: 'TDEV',
+			create_At: '2020-22-06',
+			update_At: null
+		},
+		{
+			id: 73,
+			projectSequence: 'TDEV-17',
+			summary: 'Form Validation Sitewide',
+			acceptanceCriteria: '',
+			status: 'DONE',
+			priority: 3,
+			dueDate: '2020-01-05',
+			projectIdentifier: 'TDEV',
+			create_At: '2020-23-06',
+			update_At: null
+		},
+		{
+			id: 74,
+			projectSequence: 'TDEV-18',
+			summary: 'Add Scrollbar to List when height reaches 100%',
+			acceptanceCriteria: '',
+			status: 'IN_PROGRESS',
+			priority: 3,
+			dueDate: '2020-01-07',
+			projectIdentifier: 'TDEV',
+			create_At: '2020-41-06',
+			update_At: null
+		}
+	],
+	columns: {
+		TODO: {
+			id: 'TODO',
+			title: 'To do',
+			taskIds: ['REACT-7', 'REACT-1', 'REACT-4']
+		},
+		IN_PROGRESS: {
+			id: 'IN_PROGRESS',
+			title: 'In progress',
+			taskIds: []
+		},
+		DONE: {
+			id: 'DONE',
+			title: 'Done',
+			taskIds: []
+		}
+	},
+	columnOrder: ['TODO']
+};
+
+export default data;
